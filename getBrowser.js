32d7bcf8769e34e07112c70502349bd5e288ad43
best2541 <60605549+best2541@users.noreply.github.com>
2022-01-17 15:30:26 +0700

    Update getBrowser.js

--- getBrowser.js
+++ getBrowser.js
@@ -1,32 +1,32 @@
 export function getBrowser() {
     let browser
     if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
-        return 1
+        return 'opera'
         alert('Opera');
     }
     else if (navigator.userAgent.indexOf("Edg") != -1) {
-        return 1
+        return 'edge'
         alert('Edge');
     }
     else if (navigator.userAgent.indexOf("Chrome") != -1) {
-        return 1
+        return 'chrome'
         alert('Chrome');
     }
     else if (navigator.userAgent.indexOf("Safari") != -1) {
-        return 4
+        return 'safari'
         alert('Safari');
     }
     else if (navigator.userAgent.indexOf("Firefox") != -1) {
-        return 1
+        return 'firefox'
         alert('Firefox');
     }
     else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) //IF IE > 10
     {
-        return 1
+        return 'ie'
         alert('IE');
     }
     else {
-        return 5
+        return 'unknown'
         alert('unknown');
     }
 }
